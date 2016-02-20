c8fd29daf06efe0644219518cce606a2d8e3f803
Nick Breaton <nick@breaton.com>
2016-02-19 20:22:27 -0500

    Add direction and todays problem

---
+++ 2016-02-19/solutions/nick-breaton/Easy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plain = "abcdefghijklmnopqrstuvwxyz";
+            string cipher = "ZYXWVUTSRQPONMLKJIHGFEDCBA".ToLower();
+
+            Console.Write("Enter phrase: ");
+            string input = Console.ReadLine().ToLower();
+            string conversion = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                int index = plain.IndexOf(input[i]);
+                conversion += cipher[index];
+            }
+
+            Console.WriteLine(conversion);
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# meeting-problems
+## Meeting Problems
+
+Please submit a [pull request](https://github.com/uncc-acm/meeting-problems/compare?expand=1) with your solutions.
+
+**Note:** Place solutions under `year-month-day/solutions/your-name/`
