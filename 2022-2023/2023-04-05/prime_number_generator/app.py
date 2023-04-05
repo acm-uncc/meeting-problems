0b4c26df09cc0e253a0545cbba275af95b76e225
jadenPete <jadenpete@github.com>
2023-04-05 18:50:58 -0400

    Created a Flask app that prints prime numbers

---
+++ 2022-2023/2023-04-05/prime_number_generator/app.py
@@ -0,0 +1,26 @@
+import flask
+
+app = flask.Flask(__name__)
+
+def compute_primes(n):
+	primes = []
+
+	for i in range(2, n):
+		is_prime = True
+
+		for j in range(2, i):
+			if i % j == 0:
+				is_prime = False
+
+				break
+
+		if is_prime:
+			primes.append(i)
+
+	return primes
+
+@app.route("/")
+def index():
+	n = int(flask.request.args["n"])
+
+	return flask.render_template("index.html", primes=compute_primes(n))
